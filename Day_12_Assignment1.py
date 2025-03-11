b29189304ed3e338c14abf7676f008f1dcdd8d99
Shubham Babar <122349844+ShubhamBabar22@users.noreply.github.com>
2025-03-11 13:24:21 +0530

    Add files via upload

---
+++ Day_12_Assignment1.py
@@ -0,0 +1,10 @@
+'''Write a Python program to find the number of times 4 appears in the tuple. 
+            Input: tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7 )'''
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+count_4 = tuplex.count(4)       #count() method to count occurrences
+print("Output:", count_4)
+
+#OUTPUT
+"""
+Output: 3
+"""
---
+++ Day_12_Assignment2.py
@@ -0,0 +1,12 @@
+"""Write a Python program to convert a list to a tuple. 
+Input: listx = [5, 10, 7, 4, 15, 3] 
+        Output: (5, 10, 7, 4, 15, 3)"""
+
+
+listx = [5, 10, 7, 4, 15, 3]
+tuplex = tuple(listx)
+print("Output:", tuplex)
+
+#OUTPUT:
+"""
+Output: (5, 10, 7, 4, 15, 3)"""
